6c5a98be50ffa7b55907a388f20c3b1002aa84a4
sarwin_ <64829298+SarwinR@users.noreply.github.com>
2023-05-19 17:30:48 +0400

    Dynamic Categories + Temp Category List Design

--- my_recipe/lib/home/categoryList/category_list.dart
+++ my_recipe/lib/home/categoryList/category_list.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:my_recipe/home/categoryList/widgets/category_card.dart';
+
+import '../../models/category.dart';
 
 class CategoryList extends StatefulWidget {
   const CategoryList({super.key});
@@ -8,29 +12,45 @@ class CategoryList extends StatefulWidget {
 }
 
 class _CategoryListState extends State<CategoryList> {
+  final List<FoodCategory> categories = [
+    FoodCategory(
+        name: "Mediterranean Diet",
+        imageUrl:
+            "https://images.unsplash.com/photo-1560717845-968823efbee1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+        description:
+            "A diet that emphasizes fruits, vegetables, whole grains, legumes, nuts, and seeds. It also includes moderate amounts of fish and poultry, and low amounts of red meat and processed foods.",
+        numberOfRecipes: 100),
+    FoodCategory(
+        name: "Ketogenic Diet",
+        imageUrl:
+            "https://images.unsplash.com/photo-1540420773420-3366772f4999?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=684&q=80",
+        description:
+            "A diet that is very low in carbohydrates and high in fat. It is designed to help people lose weight by putting their bodies into a state of ketosis, where they burn fat for energy instead of carbohydrates.",
+        numberOfRecipes: 100),
+    FoodCategory(
+        name: "Vegan Diet",
+        imageUrl:
+            "https://images.unsplash.com/photo-1543362906-acfc16c67564?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=765&q=80",
+        description:
+            "A diet that excludes all animal products, including meat, poultry, seafood, eggs, and dairy.",
+        numberOfRecipes: 100),
+    FoodCategory(
+        name: "Paleo Diet",
+        imageUrl:
+            "https://images.unsplash.com/photo-1551028150-64b9f398f678?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80",
+        description:
+            "A diet that is based on the foods that our hunter-gatherer ancestors ate. It is high in protein and low in carbohydrates, and it excludes processed foods, grains, legumes, and dairy.",
+        numberOfRecipes: 100),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       physics: NeverScrollableScrollPhysics(),
       shrinkWrap: true,
-      itemCount: 10,
+      itemCount: categories.length,
       itemBuilder: (context, index) {
-        return Card(
-          elevation: 0,
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-          child: ListTile(
-            leading: CircleAvatar(
-              radius: 30,
-              backgroundColor: Theme.of(context).colorScheme.primaryContainer,
-              child: Icon(Icons.fastfood_outlined,
-                  color: Theme.of(context).colorScheme.primary),
-            ),
-            title: Text("Category ${index + 1}"),
-            subtitle: Text("Category ${index + 1}"),
-            trailing: Icon(Icons.arrow_forward_ios_outlined),
-          ),
-        );
+        return CategoryCard(category: categories[index]);
       },
     );
   }
--- my_recipe/lib/home/categoryList/widgets/category_card.dart
+++ my_recipe/lib/home/categoryList/widgets/category_card.dart
@@ -1,1 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:my_recipe/models/category.dart';
 
+class CategoryCard extends StatefulWidget {
+  const CategoryCard({super.key, required this.category});
+
+  final FoodCategory category;
+
+  @override
+  State<CategoryCard> createState() => _CategoryCardState();
+}
+
+class _CategoryCardState extends State<CategoryCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(children: [
+      Container(
+        margin: EdgeInsets.only(bottom: 10, left: 20, right: 20),
+        height: MediaQuery.of(context).size.height * 0.15,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20.0),
+            image: DecorationImage(
+                image: NetworkImage(widget.category.imageUrl),
+                fit: BoxFit.cover)),
+      ),
+      Container(
+        margin: EdgeInsets.only(bottom: 10, left: 20, right: 20),
+        height: MediaQuery.of(context).size.height * 0.15,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20.0),
+            color: Theme.of(context).colorScheme.surface.withOpacity(0.7)),
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  widget.category.name,
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+                Text(
+                  widget.category.description.length > 70
+                      ? widget.category.description.substring(0, 70) + '...'
+                      : widget.category.description,
+                  style: Theme.of(context).textTheme.bodyMedium,
+                ),
+              ]),
+        ),
+      ),
+    ]);
+  }
+}
