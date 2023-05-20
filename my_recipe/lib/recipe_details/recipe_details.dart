267d9f8256a280aafb399177ed5552ab50e8a05e
Farhaan Nazirkhan <96138084+nfarhaan@users.noreply.github.com>
2023-05-20 23:52:53 +0400

    Update recipe_details.dart

--- my_recipe/lib/recipe_details/recipe_details.dart
+++ my_recipe/lib/recipe_details/recipe_details.dart
@@ -116,6 +116,10 @@ class RecipeDetails extends StatelessWidget {
                         SizedBox(
                           height: 24,
                         ),
+                      Container(
+                        margin: EdgeInsets.only( left: 8, right: 8),
+                        child: Column(
+                          children:[
 
                         Text(
                           "About Recipe",
@@ -130,7 +134,7 @@ class RecipeDetails extends StatelessWidget {
                         Text(
                           "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
                           style: GoogleFonts.roboto(
-                              fontWeight: FontWeight.w300, color: Colors.grey),
+                              fontWeight: FontWeight.w300, color: Colors.grey),textAlign: TextAlign.justify,
                         ),
 
                         SizedBox(
@@ -150,8 +154,10 @@ class RecipeDetails extends StatelessWidget {
                         Text(
                           "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam gravida mauris ut semper posuere. Fusce volutpat neque non elementum rutrum. Phasellus et lacus et lacus accumsan commodo ac quis felis. Proin quam lorem, pellentesque in pretium ac, convallis et massa. Donec in nisi magna. In rutrum, nibh id interdum auctor, nisl elit ultricies arcu, ut ullamcorper ipsum mauris quis eros. Aenean ex tortor, pretium eu vulputate a, consectetur vel diam. Cras nulla nisl, euismod eu purus ac, sagittis viverra elit. Duis pulvinar sem quis fermentum iaculis. Quisque pharetra turpis velit, ut hendrerit tortor finibus id. Sed eu pretium libero.Vestibulum semper justo eu purus suscipit scelerisque. Proin eget rhoncus ex. Nam eget egestas nisi. Morbi molestie imperdiet sapien, ut ornare est laoreet in. Nam fermentum venenatis lorem, et scelerisque ante fermentum eget. Vestibulum eget urna ullamcorper, consequat odio et, efficitur arcu. Ut molestie feugiat tristique. Ut lectus erat, malesuada eget elit et, laoreet commodo augue. Nullam at velit vel ipsum tristique rhoncus. Fusce euismod facilisis lorem nec consequat. Donec in turpis tellus. Nunc at massa et sapien hendrerit volutpat. Curabitur condimentum, nibh hendrerit fermentum suscipit, justo lacus pellentesque felis, vitae rhoncus mi est sit amet massa. Donec id hendrerit felis, at blandit ex. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
                           style: GoogleFonts.roboto(
-                              fontWeight: FontWeight.w300, color: Colors.grey),
+                              fontWeight: FontWeight.w300, color: Colors.grey),textAlign: TextAlign.justify,
                         ),
+                          ])
+                      )
             // SizedBox(height: 16.0),
             // Text(
             //   "Ingredients",
