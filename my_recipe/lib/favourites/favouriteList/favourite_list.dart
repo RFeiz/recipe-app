bc6285737ab5b6449e56e67edc966678c975f462
RFeiz <zief002@gmail.com>
2023-05-27 00:09:10 +0400

    Fetching favourites p1

--- my_recipe/lib/favourites/favouriteList/favourite_list.dart
+++ my_recipe/lib/favourites/favouriteList/favourite_list.dart
@@ -1,82 +1,73 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:my_recipe/favourites/favouriteList/widgets/favourite_card.dart';
 import 'package:my_recipe/models/food.dart';
+import 'package:my_recipe/user.dart';
 
 class FavouriteList extends StatefulWidget {
   final bool isDescending;
   final String selectedSortOptions;
 
+  // ignore: prefer_const_constructors_in_immutables
   FavouriteList(
       {Key? key, required this.isDescending, required this.selectedSortOptions})
       : super(key: key);
-
-  final List<Food> favouriteList = [
-    Food(
-        name: "Pasta with Tomato Sauce",
-        smallDescription:
-            "A delicious pasta with tomato sauce and cheese topping.",
-        longDescription:
-            "A delicious pasta with tomato sauce and cheese topping.",
-        thumbnailUrl:
-            "https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
-        cookingTime: "30m",
-        calories: 596,
-        likes: 5,
-        ingredientList: []),
-    Food(
-        name: "Beef Wellington",
-        smallDescription:
-            "Beef tenderloin wrapped in layers of pâté, duxelles, parma ham, and puff pastry, then baked.",
-        longDescription: "Beef with a crust of puff pastry.",
-        thumbnailUrl:
-            "https://images.unsplash.com/photo-1675718341348-65224936b742?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1115&q=80",
-        cookingTime: "4h",
-        calories: 427,
-        likes: 256302,
-        ingredientList: []),
-    Food(
-        name: "Ramen Noodles",
-        smallDescription:
-            "A chinese noodle dish with a fish-based broth and a variety of toppings.",
-        longDescription:
-            "A chinese noodle dish with a fish-based broth and a variety of toppings.",
-        thumbnailUrl:
-            "https://images.unsplash.com/photo-1569718212165-3a8278d5f624?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=880&q=80",
-        cookingTime: "15m",
-        calories: 450,
-        likes: 785000000,
-        ingredientList: []),
-  ];
 
   @override
   State<FavouriteList> createState() => _FavouriteListState();
 }
 
 class _FavouriteListState extends State<FavouriteList> {
+  List<Food> favouriteList = [];
+
+  String Id = currentId;
+
+  Future getFavouriteList() async {
+    await FirebaseFirestore.instance
+        .collection("users")
+        .get()
+        .then((QuerySnapshot QuerySnapshot) {
+      favouriteList.clear();
+      QuerySnapshot.docs.forEach((doc) {
+        Food food = Food(
+          name: doc["name"],
+          smallDescription: doc["small_description"],
+          longDescription: doc["long_description"],
+          thumbnailUrl: doc["thumbnail_url"],
+          cookingTime: doc["cooking_time"],
+          calories: doc["calories"],
+          likes: doc["likes"],
+          ingredientList: [],
+        );
+        favouriteList.add(food);
+      });
+    });
+  }
+
   List<Food> get sortedList {
     switch (widget.selectedSortOptions) {
       case "Name":
-        return widget.favouriteList
+        return favouriteList
           ..sort((item1, item2) => widget.isDescending
               ? item2.name.compareTo(item1.name)
               : item1.name.compareTo(item2.name));
       case "Cooking Time":
-        return widget.favouriteList
+        return favouriteList
           ..sort((item1, item2) => widget.isDescending
               ? item2.cookingTime.compareTo(item1.cookingTime)
               : item1.cookingTime.compareTo(item2.cookingTime));
       case "Calories":
-        return widget.favouriteList
+        return favouriteList
           ..sort((item1, item2) => widget.isDescending
               ? item2.calories.compareTo(item1.calories)
               : item1.calories.compareTo(item2.calories));
       case "Likes":
-        return widget.favouriteList
+        return favouriteList
           ..sort((item1, item2) => widget.isDescending
               ? item2.likes.compareTo(item1.likes)
               : item1.likes.compareTo(item2.likes));
       default:
-        return widget.favouriteList
+        return favouriteList
           ..sort((item1, item2) => widget.isDescending
               ? item2.name.compareTo(item1.name)
               : item1.name.compareTo(item2.name));
@@ -85,13 +76,17 @@ class _FavouriteListState extends State<FavouriteList> {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      physics: const NeverScrollableScrollPhysics(),
-      shrinkWrap: true,
-      itemCount: widget.favouriteList.length,
-      itemBuilder: (_, i) {
-        return FavouriteCard(food: sortedList[i]);
-      },
-    );
+    return FutureBuilder(
+        future: getFavouriteList(),
+        builder: (context, snapshot) {
+          return ListView.builder(
+            physics: const NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            itemCount: favouriteList.length,
+            itemBuilder: (_, i) {
+              return FavouriteCard(food: sortedList[i]);
+            },
+          );
+        });
   }
 }
