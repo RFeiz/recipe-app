cc664c308f57eaaa27e11dfc5a03d62d8b871f3c
levynkhs <96076166+levynkwong2001@users.noreply.github.com>
2023-06-04 13:17:04 +0400

    login check

--- my_recipe/lib/main.dart
+++ my_recipe/lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:dynamic_color/dynamic_color.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:my_recipe/login/loginPage.dart';
 
 import 'package:my_recipe/models/custom_query.dart';
 import 'package:my_recipe/main_view.dart';
+
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,7 +15,7 @@ Future<void> main() async {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -22,22 +25,31 @@ class MyApp extends StatelessWidget {
     return DynamicColorBuilder(
       builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
         return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            title: 'My Recipes',
-            themeMode: ThemeMode.system,
-            theme: ThemeData(
-              colorScheme: ColorScheme.fromSeed(
-                seedColor: lightDynamic?.primary ?? Colors.green,
-              ),
-              useMaterial3: true,
+          debugShowCheckedModeBanner: false,
+          title: 'My Recipes',
+          themeMode: ThemeMode.system,
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(
+              seedColor: lightDynamic?.primary ?? Colors.green,
             ),
-            darkTheme: ThemeData(
-              colorScheme: ColorScheme.fromSeed(
-                  seedColor: darkDynamic?.primary ?? Colors.green,
-                  brightness: Brightness.dark),
-              useMaterial3: true,
+            useMaterial3: true,
+          ),
+          darkTheme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(
+              seedColor: darkDynamic?.primary ?? Colors.green,
+              brightness: Brightness.dark,
             ),
-            home: const MainView());
+            useMaterial3: true,
+          ),
+          // Determine the initial route based on authentication state
+          initialRoute: FirebaseAuth.instance.currentUser == null
+              ? '/login'
+              : '/main',
+          routes: {
+            '/login': (context) => const LoginPage(), // Set the LoginPage as a route
+            '/main': (context) => const MainView(),
+          },
+        );
       },
     );
   }
