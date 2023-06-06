a502f73c447c628a8aa6d2cab431c1a5cbfcb020
sarwin_ <64829298+SarwinR@users.noreply.github.com>
2023-06-06 11:39:00 +0400

    Fixed theme issue with dots on login page

--- my_recipe/lib/login/introductionWidget.dart
+++ my_recipe/lib/login/introductionWidget.dart
@@ -53,18 +53,19 @@ class _IntroductionWidgetState extends State<IntroductionWidget>
 
   @override
   Widget build(BuildContext context) {
-     const bodyStyle = TextStyle(fontSize: 19.0);
-  final pageDecoration = PageDecoration(
-    titleTextStyle: TextStyle(
-      fontSize: 28.0,
-      fontWeight: FontWeight.w700,
-      color: Theme.of(context).colorScheme.onSurface,
-    ),
-    bodyTextStyle: bodyStyle,
-    bodyPadding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
-    pageColor: Colors.transparent, // Use transparent color for the page background
-    imagePadding: EdgeInsets.zero,
-  );
+    const bodyStyle = TextStyle(fontSize: 19.0);
+    final pageDecoration = PageDecoration(
+      titleTextStyle: TextStyle(
+        fontSize: 28.0,
+        fontWeight: FontWeight.w700,
+        color: Theme.of(context).colorScheme.onSurface,
+      ),
+      bodyTextStyle: bodyStyle,
+      bodyPadding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
+      pageColor:
+          Colors.transparent, // Use transparent color for the page background
+      imagePadding: EdgeInsets.zero,
+    );
 
     return IntroductionScreen(
       key: introKey,
@@ -77,8 +78,6 @@ class _IntroductionWidgetState extends State<IntroductionWidget>
         alignment: Alignment.topRight,
       ),
       pages: [
-        
-
         PageViewModel(
           title: "",
           bodyWidget: Column(
@@ -86,11 +85,14 @@ class _IntroductionWidgetState extends State<IntroductionWidget>
             children: [
               _buildImage('1.png'), // Place the image widget here
               const SizedBox(height: 30), // Add spacing between image and title
-               Column(
+              Column(
                 children: [
                   Text(
                     'Ready set go, Time to cook',
-                     style: Theme.of(context).textTheme.titleLarge!.copyWith(fontWeight: FontWeight.bold),
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleLarge!
+                        .copyWith(fontWeight: FontWeight.bold),
                   ),
                   SizedBox(height: 10),
                   Text(
@@ -100,7 +102,6 @@ class _IntroductionWidgetState extends State<IntroductionWidget>
                   ),
                 ],
               ),
-              
             ],
           ),
           decoration: pageDecoration.copyWith(
@@ -111,11 +112,11 @@ class _IntroductionWidgetState extends State<IntroductionWidget>
           ),
           image: SlideTransition(
             position: _slideAnimation,
-            child: const SizedBox(), // Keep the image widget as an empty SizedBox
+            child:
+                const SizedBox(), // Keep the image widget as an empty SizedBox
           ),
           reverse: true,
         ),
-
         PageViewModel(
           title: "",
           bodyWidget: Column(
@@ -123,21 +124,23 @@ class _IntroductionWidgetState extends State<IntroductionWidget>
             children: [
               _buildImage('3.png'), // Place the image widget here
               const SizedBox(height: 30), // Add spacing between image and title
-               Column(
+              Column(
                 children: [
                   Text(
                     'Follow the Instruction',
-                    style: Theme.of(context).textTheme.titleLarge!.copyWith(fontWeight: FontWeight.bold),
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleLarge!
+                        .copyWith(fontWeight: FontWeight.bold),
                   ),
                   SizedBox(height: 10),
                   Text(
                     'Unleash your culinary creativity and elevate your cooking game with this inspiring recipe app.',
-                   style: Theme.of(context).textTheme.bodyLarge,
+                    style: Theme.of(context).textTheme.bodyLarge,
                     textAlign: TextAlign.justify,
                   ),
                 ],
               ),
-              
             ],
           ),
           decoration: pageDecoration.copyWith(
@@ -148,22 +151,26 @@ class _IntroductionWidgetState extends State<IntroductionWidget>
           ),
           image: SlideTransition(
             position: _slideAnimation,
-            child: const SizedBox(), // Keep the image widget as an empty SizedBox
+            child:
+                const SizedBox(), // Keep the image widget as an empty SizedBox
           ),
           reverse: true,
         ),
-       PageViewModel(
+        PageViewModel(
           title: "",
           bodyWidget: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               _buildImage('2.png'), // Place the image widget here
               const SizedBox(height: 30), // Add spacing between image and title
-               Column(
+              Column(
                 children: [
                   Text(
                     'Cook your food',
-                     style: Theme.of(context).textTheme.titleLarge!.copyWith(fontWeight: FontWeight.bold),
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleLarge!
+                        .copyWith(fontWeight: FontWeight.bold),
                   ),
                   SizedBox(height: 10),
                   Text(
@@ -173,7 +180,6 @@ class _IntroductionWidgetState extends State<IntroductionWidget>
                   ),
                 ],
               ),
-              
             ],
           ),
           decoration: pageDecoration.copyWith(
@@ -184,11 +190,12 @@ class _IntroductionWidgetState extends State<IntroductionWidget>
           ),
           image: SlideTransition(
             position: _slideAnimation,
-            child: const SizedBox(), // Keep the image widget as an empty SizedBox
+            child:
+                const SizedBox(), // Keep the image widget as an empty SizedBox
           ),
           reverse: true,
         ),
-         PageViewModel(
+        PageViewModel(
           title: "",
           bodyWidget: Column(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -199,17 +206,19 @@ class _IntroductionWidgetState extends State<IntroductionWidget>
                 children: [
                   Text(
                     'Time to eat',
-                     style: Theme.of(context).textTheme.titleLarge!.copyWith(fontWeight: FontWeight.bold),
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleLarge!
+                        .copyWith(fontWeight: FontWeight.bold),
                   ),
                   SizedBox(height: 10),
                   Text(
                     'Treat yourself to a homemade feast, crafted with love and care, ready to satisfy your hunger and delight your senses.',
-                     style: Theme.of(context).textTheme.bodyLarge,
+                    style: Theme.of(context).textTheme.bodyLarge,
                     textAlign: TextAlign.justify,
                   ),
                 ],
               ),
-              
             ],
           ),
           decoration: pageDecoration.copyWith(
@@ -220,7 +229,8 @@ class _IntroductionWidgetState extends State<IntroductionWidget>
           ),
           image: SlideTransition(
             position: _slideAnimation,
-            child: const SizedBox(), // Keep the image widget as an empty SizedBox
+            child:
+                const SizedBox(), // Keep the image widget as an empty SizedBox
           ),
           reverse: true,
         ),
@@ -230,11 +240,12 @@ class _IntroductionWidgetState extends State<IntroductionWidget>
       showNextButton: false, // Remove the next button
       showBackButton: false, // Remove the back button
       showDoneButton: false, // Remove the done button
-      dotsDecorator: const DotsDecorator(
+      dotsDecorator: DotsDecorator(
         size: Size(10.0, 10.0),
-        color: Color(0xFFBDBDBD),
+        color: Theme.of(context).colorScheme.onSurface.withOpacity(0.4),
         activeSize: Size(22.0, 10.0),
-        activeColor: Color(0xFF575ecb), // Color of active dot
+        activeColor:
+            Theme.of(context).colorScheme.primary, // Color of active dot
         activeShape: RoundedRectangleBorder(
           borderRadius: BorderRadius.all(Radius.circular(25.0)),
         ),
